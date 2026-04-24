0f09f79646fe20c93b55ba04d8ce7168016214cd
LeonardoVanini <ofs699@student.edu.ti.ch>
2026-04-24 16:05:21 +0200

    add Attività

--- 2_Applicativo/Sviluppo/Applicazione/MyRoutineNew/Models/Attivita.cs
+++ 2_Applicativo/Sviluppo/Applicazione/MyRoutineNew/Models/Attivita.cs
@@ -8,6 +8,37 @@ namespace MyRoutineNew.Models
 {
     public class Attivita
     {
-        private
+        private string _nome;
+        private string _descrizione;
+        private DateTime _dateTimeInizio;
+        private DateTime _dateTimeFine;
+        private string _categoria;
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+        public string Descrizione
+        {
+            get { return _descrizione; }
+            set { _descrizione = value; }
+        }
+        public DateTime DateTimeInizio
+        {
+            get { return _dateTimeInizio; }
+            set { _dateTimeInizio = value; }
+        }
+        public DateTime DateTimeFine
+        {
+            get { return _dateTimeFine; }
+            set { _dateTimeFine = value; }
+        }
+        public string Categoria
+        {
+            get { return _categoria; }
+            set { _categoria = value; }
+        }
+
     }
 }
